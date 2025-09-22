d7ef2f5434230b6eeef8d687c8231798b5506397
Jae-YS <jae.jy.seo@gmail.com>
2025-09-22 16:13:24 -0400

    API URL Change

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL_LOCAL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
   withCredentials: true,
 });
