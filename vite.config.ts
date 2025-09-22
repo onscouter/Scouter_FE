833fbbded552fb4000e9cc82b90721447f5f2133
Jae-YS <jae.jy.seo@gmail.com>
2025-09-21 22:48:34 -0400

    fixing vite.config bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve, dirname } from "path";
-import { fileURLToPath } from "url";
 import { visualizer } from "rollup-plugin-visualizer";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), visualizer({ open: true })],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
